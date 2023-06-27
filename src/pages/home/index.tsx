e108bb31fa68f2815673ee7e73548d6455c5c398
“muddyrain” <qiu3291002845@gmail.com>
2023-06-27 22:22:09 +0800

    chore(custom): 强制 commitlint 校验

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,12 @@
 import './index.css'
-import '@/service/index'
 import { Former } from '@/components'
-import { useEffect } from 'react'
-import { getDogApi } from '@/api'
 export const HomePage = () => {
   const [$form] = Former.useForm()
-  useEffect(() => {
-    getDogApi()
-  }, [])
   return (
     <>
       <Former
         form={$form}
-        onSubmit={values => {
+        onSubmit={(values: any) => {
           console.log(values)
         }}
         dataSource={[
