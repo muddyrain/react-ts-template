62da25e612d07049b7f071cc72b32ec84beae389
“muddyrain” <qiu3291002845@gmail.com>
2023-09-05 10:14:30 +0800

    style(custom): 移除未启用的api

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,4 @@
-import { getDogApi } from '@/api'
-import './index.css'
-import { useEffect } from 'react'
 export const HomePage = () => {
-  useEffect(() => {
-    getDogApi()
-  }, [])
   return (
     <>
       <span>欢迎回来</span>
