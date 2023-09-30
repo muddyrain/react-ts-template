ca3ae64c86b4ee7e18476213181d4e4cf39f6985
muddyrain <qiu3291002845@gmail.com>
2023-09-30 21:10:41 +0800

    style(登录): 修改登录结构以及登录方式

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -35,22 +35,8 @@ export const LoginPage: FC = () => {
     <div className={styles.container}>
       <img className="absolute right-0 top-0 w-[8%] h-[15%]" src={images.RightTopImage} />
       <img className="absolute left-0 top-[15%] w-[5%] h-[20%]" src={images.LeftCenterTopImage} />
-      <div className=" absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] m-auto 2xl:w-[1380px] 2xl:h-[750px]  xl:w-[1180px] xl:h-[650px]  md:w-[960px] md:h-[520px] duration-300 overflow-hidden rounded-lg flex">
-        <div className={`${styles.leftBox} w-1/2 relative p-16`}>
-          <img
-            src={images.ContentImage}
-            className="absolute bottom-[5%] w-[80%] h-[60%] left-[10%]"
-            alt=""
-          />
-          <div className="relative z-1 w-[75%]">
-            <div className="line bg-white w-14 h-2"></div>
-            <div className="text-white mt-6 2xl:text-5xl xl:text-4xl md:text-3xl">专业数据分析</div>
-            <div className="text-white mt-6 xl:text-base md:text-sm tracking-widest">
-              数据分析是指用适当的统计分析方法对收集来的大量数据进行分析，将它们加以汇总和理解并消化，以求最大化地开发数据的功能，发挥数据的作用。
-            </div>
-          </div>
-        </div>
-        <div className={`w-1/2 bg-white relative p-24 ${styles.rightBox}`}>
+      <div className=" absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] m-auto w-[960px] h-[640px] duration-300 overflow-hidden rounded-lg flex">
+        <div className={` w-full bg-white relative p-24 ${styles.loginBox}`}>
           <img className="absolute top-0 left-0 h-[6%] w-[25%]" src={images.LoginTopImage} alt="" />
           <img
             className="absolute bottom-0 left-0 w-full h-[25%]"
@@ -64,7 +50,13 @@ export const LoginPage: FC = () => {
                 <span className="line bottom-[-5%] z-0 left-[-10%] absolute w-[120%] h-2 bg-blue-100"></span>
               </div>
             </div>
-            <div className="login xl:mt-20 md:mt-10">
+            <form
+              className="login my-20"
+              onSubmit={e => {
+                e.preventDefault()
+                handleLogin()
+              }}
+            >
               <div className={`${styles.input_box}`}>
                 <img src={images.UserImage} alt="" className={`${styles.icon}`} />
                 <input
@@ -107,14 +99,15 @@ export const LoginPage: FC = () => {
                   )}
                 </div>
               </div>
-              <div
-                className={`bg-blue-600 xl:mt-16 md:8 rounded-[37px] duration-300 py-6 text-center text-white cursor-pointer hover:bg-blue-400`}
-                onClick={handleLogin}
+
+              <button
+                className={`bg-blue-600 rounded-[37px] duration-300 py-6 text-center text-white cursor-pointer hover:bg-blue-400 w-full mt-10 border-0`}
+                type="submit"
               >
                 {state.loading && <LoadingOutlined />}
                 <span className="ml-2">登录</span>
-              </div>
-            </div>
+              </button>
+            </form>
           </div>
         </div>
       </div>
