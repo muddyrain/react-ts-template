cabf812a2f495251fdbf5b2f9e5934de70323916
muddyrain <qiu3291002845@gmail.com>
2023-10-13 22:50:30 +0800

    chore(login): 移除未引用的图片资源

--- src/pages/login/images/index.ts
+++ src/pages/login/images/index.ts
@@ -1,6 +1,5 @@
 import RightTopImage from './right-top.png'
 import LeftCenterTopImage from './left-center-top.png'
-import ContentImage from './content_img.png'
 import BottomImage from './bottom.png'
 import UserImage from './user.png'
 import PasswordImage from './password.png'
@@ -10,7 +9,6 @@ import LoginTopImage from './login-t.png'
 export default {
   RightTopImage,
   LeftCenterTopImage,
-  ContentImage,
   BottomImage,
   LoginBottomImage,
   LoginTopImage,
